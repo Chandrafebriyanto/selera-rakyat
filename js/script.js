85a6b969828c1f7b3247678353d6e978c2510277
027candrafebriyanto <febriyantocandra05@gmail.com>
2025-10-15 23:13:16 +0700

    commit

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// tittle: Navbar Sticky
 $(document).ready(function () {
   $(window).scroll(function () {
     var header = $("header");
@@ -6,5 +7,29 @@ $(document).ready(function () {
     } else {
       header.removeClass("sticky");
     }
+  });
+});
+
+// Tittle: Hamburger Menu
+$(document).ready(function () {
+  // (Kode Scroll Sticky yang sudah ada di sini)
+  $(window).scroll(function () {
+    var header = $("header");
+    if ($(window).scrollTop() > 100) {
+      header.addClass("sticky");
+    } else {
+      header.removeClass("sticky");
+    }
+  });
+
+
+  // note: Program Hamburger Menu
+  $("#hamburger-icon").click(function () {
+    $("#mobile-menu").toggleClass("active");
+  });
+
+  // note: Tutup menu saat link diklik
+  $("#mobile-menu a").click(function () {
+    $("#mobile-menu").removeClass("active");
   });
 });

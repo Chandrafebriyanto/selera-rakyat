5349c027815321823424efa3b8076948420c5c93
027candrafebriyanto <febriyantocandra05@gmail.com>
2025-10-15 22:27:09 +0700

    commit

--- js/script.js
+++ js/script.js
@@ -1,8 +1,6 @@
 $(document).ready(function () {
   $(window).scroll(function () {
     var header = $("header");
-    // Navbar akan berubah setelah scroll sejauh 200px dari atas
-    // Anda bisa mengubah angka 200 ini jika dirasa terlalu cepat/lambat
     if ($(window).scrollTop() > 100) {
       header.addClass("sticky");
     } else {

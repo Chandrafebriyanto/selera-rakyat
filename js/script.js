13d295ed83e8c0499b758f6ce94fbb344c458e73
027candrafebriyanto <febriyantocandra05@gmail.com>
2025-10-16 13:19:06 +0700

    commit

--- js/script.js
+++ js/script.js
@@ -1,18 +1,5 @@
-// tittle: Navbar Sticky
-$(document).ready(function () {
-  $(window).scroll(function () {
-    var header = $("header");
-    if ($(window).scrollTop() > 100) {
-      header.addClass("sticky");
-    } else {
-      header.removeClass("sticky");
-    }
-  });
-});
-
 // Tittle: Hamburger Menu
 $(document).ready(function () {
-  // (Kode Scroll Sticky yang sudah ada di sini)
   $(window).scroll(function () {
     var header = $("header");
     if ($(window).scrollTop() > 100) {
